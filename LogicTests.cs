6f29fca4e0cf4fa82c86ecefec8574d7445b9517
dragonipam <101445162+dragonipam@users.noreply.github.com>
2022-09-11 18:00:04 +0800

    Add files via upload

---
+++ LogicTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Z2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Z2.Tests
+{
+    [TestClass()]
+    public class LogicTests
+    {
+        [TestMethod()]
+        public void InputErrorMoreTrue()
+        {
+            Boolean mistake = Logic.IsMistake(6);
+            Assert.AreEqual(true, mistake);
+        }
+
+        [TestMethod()]
+        public void InputErrorLessTrue()
+        {
+            Boolean mistake = Logic.IsMistake(-15);
+            Assert.AreEqual(true, mistake);
+        }
+
+        [TestMethod()]
+        public void InputErrorFalse()
+        {
+            Boolean mistake = Logic.IsMistake(4);
+            Assert.AreEqual(false, mistake);
+        }
+    }
+}
